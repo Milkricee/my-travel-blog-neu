431b0deaa47acf639d5f90b2394628df14e393b0
Milkricee <jnr.daniil@gmail.com>
2024-11-26 23:33:41 +0100

    Refactor Firebase configuration to log the config object for debugging purposes

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log("Penis penis:", firebaseConfig);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
