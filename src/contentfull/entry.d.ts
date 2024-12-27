688e7ab795a34b147750a0b0eb59ff6239c68271
Milkricee <jnr.daniil@gmail.com>
2024-12-27 19:05:00 +0100

    Add Contentful rich text support and restructure content model; remove Kolumbien page

--- src/contentfull/entry.d.ts
+++ src/contentfull/entry.d.ts
@@ -1,14 +1,17 @@
 export interface ContentfulEntry {
-    title: string;
-    publishedDate: string;
-    content: string;
-    featuredImage?: {
-      fields: {
-        file: {
-          url: string;
-        };
-        title: string;
+  title: string;
+  publishedDate: string;
+  content1?: string;
+  content2?: string;
+  content3?: string;
+  content4?: string;
+  content5?: string;
+  fotos?: Array<{
+    fields: {
+      file: {
+        url: string;
       };
+      title: string;
     };
-  }
-  
+  }>;
+}
