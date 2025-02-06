d71079fd4a2eb969c5351a2c22ef17f2aa690639
Milkricee <jnr.daniil@gmail.com>
2025-02-06 22:38:44 +0100

    Enhance sitemap configuration and update package.json scripts for better sitemap management

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://dan-travels.com',
-  generateRobotsTxt: true, // Erstellt auch eine robots.txt
+  generateRobotsTxt: true, // Erstellt auch robots.txt
   sitemapSize: 5000,
+  exclude: ['/admin', '/secret'], // Falls du Seiten ausschließen willst
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [
+      'https://dan-travels.com/server-sitemap.xml', // Falls du API-Routen hast
+    ],
+  },
 };
