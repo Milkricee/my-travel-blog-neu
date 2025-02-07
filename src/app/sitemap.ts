14905f1bd01ddb4b787056831979c72ce7d55a31
Milkricee <jnr.daniil@gmail.com>
2025-02-07 14:32:22 +0100

    Add additional URLs to sitemap for improved SEO and navigation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,5 +6,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://dan-travels.com/",
       lastModified: new Date().toISOString(),
     },
+    {
+      url: "https://dan-travels.com/about",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://dan-travels.com/kolumbien",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://dan-travels.com/senegal",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://dan-travels.com/marokko",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://dan-travels.com/nicaragua",
+      lastModified: new Date().toISOString(),
+    },
+     {
+      url: "https://dan-travels.com/datenschutz",
+      lastModified: new Date().toISOString(),
+    },
   ];
 }
