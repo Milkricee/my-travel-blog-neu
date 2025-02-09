7756bcc5bb9f9a5f4bf7c645e13045854f686acb
Milkricee <jnr.daniil@gmail.com>
2025-02-09 21:22:52 +0100

    Remove Contentful integration and related components from the project

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net", // Erlaube Bilder von Contentful
-        pathname: "/**", // Erlaubt alle Bilder von dieser Domain
-      },
-    ],
-  },
   webpack: (config) => {
     config.resolve.modules.push("node_modules");
     return config;
