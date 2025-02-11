ad3e3729f21e468ff8d0d5596c8d0397091e210f
Milkricee <jnr.daniil@gmail.com>
2025-02-11 16:47:03 +0100

    Disable robots.txt generation in sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://dan-travels.com',
-  generateRobotsTxt: true, // Erstellt auch robots.txt
+  generateRobotsTxt: false, // Erstellt auch robots.txt
   sitemapSize: 5000,
   generateIndexSitemap: false, // Verhindert separate Index-Sitemaps
   exclude: ['/admin', '/secret'], // Falls du Seiten ausschließen willst
